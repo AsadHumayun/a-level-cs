2820ab08a18144f7949ed617778ed21fcbaa47ea
Asad <asad@asadh.dev>
2022-11-04 10:44:20 +0000

    Add string "exercises"

---
+++ A2/Py Exercises/PyNative String Exercises/1A.py
@@ -0,0 +1,4 @@
+string = 'string'
+print(string[0], string[
+    round(len(string) / 2)
+], string[len(string) - 1])
---
+++ A2/Py Exercises/PyNative String Exercises/1B.py
@@ -0,0 +1,5 @@
+string = "string"
+middle = int(len(string) / 2)
+
+result = string[middle - 1] + string[middle] + string[middle + 1]
+print(result) #E: rin
