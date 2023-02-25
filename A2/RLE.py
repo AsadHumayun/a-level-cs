9d2b0795e6dc34f05d23c4e5fd7c2c3c51b820f0
Asad <asad@asadh.dev>
2023-02-25 16:24:46 +0000

    Optimise RLE python file

--- A2/RLE.py
+++ A2/RLE.py
@@ -6,13 +6,11 @@ lastChar = ''
 
 for char in list(text):
     if char == lastChar:
-        if rle[len(rle) - 1][0] == char:
-            rle[len(rle) - 1] = [char, rle[len(rle) - 1][1] + 1]
-            lastChar = char
-
+        rle[len(rle) - 1] = [char, rle[len(rle) - 1][1] + 1]
     else:
         rle.append([char, 1])
-        lastChar = char
+
+    lastChar = char
 
 
 print('\n'.join(map(lambda x: x[0] + ': ' + str(x[1]), rle)))
